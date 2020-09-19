3234cec971b914f02e23e453bd67796dd20ab32c
Igor Golovin <lislon@mail.ru>
2020-09-19 14:44:06 +0300

    Debug tests on CI 7

--- src/util/moment-msk.ts
+++ src/util/moment-msk.ts
@@ -2,4 +2,4 @@ import moment from 'moment'
 import 'moment-timezone/index'
 
 
-export const mskMoment = (...args: string[]) => moment(...args).tz('Europe/Moscow').locale('en')
+export const mskMoment = (...args: string[]) => moment(...args).tz('Europe/Moscow', true).locale('en')
