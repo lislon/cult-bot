ef4a253456aa9774865694d9d614af2cca2d0155
Igor Golovin <lislon@mail.ru>
2020-09-19 13:50:02 +0300

    Fix build error

--- src/lib/timetable/test2.ts
+++ src/lib/timetable/test2.ts
@@ -1,9 +1,11 @@
-import { cleanText } from './timetable-utils'
 import { predictIntervals } from './intervals';
 import { parseTimetable } from './parser'
-import moment = require('moment')
 import { mskMoment } from '../../util/moment-msk'
 
 const timetable = parseTimetable('в любое время')
 // console.log(JSON.stringify(timetable, undefined, 2));
-console.log(JSON.stringify(predictIntervals(mskMoment('2020-01-01 00:00:00'), timetable.value)));
+if (timetable.status === true) {
+    console.log(JSON.stringify(predictIntervals(mskMoment('2020-01-01 00:00:00'), timetable.value)));
+} else {
+    console.log('fail')
+}
