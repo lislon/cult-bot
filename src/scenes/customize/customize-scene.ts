2102221e77b2473e2d4d2cdcb02ca2beff5c2045
Igor Golovin <lislon@users.noreply.github.com>
2020-11-18 02:03:12 +0300

    Add museums menu

--- src/scenes/customize/customize-scene.ts
+++ src/scenes/customize/customize-scene.ts
@@ -177,6 +177,9 @@ async function getKeyboardOblasti(ctx: ContextMessageUpdate) {
         ...(menu.dropDownButtons('menu_exhibitions',
             getSectionFromI18n(`exhibitions`)
         )),
+        ...(menu.dropDownButtons('menu_museums,
+            getSectionFromI18n(`museums`)
+        )),
         ...(menu.dropDownButtons('menu_theaters',
             getSectionFromI18n(`theaters`)
         )),
