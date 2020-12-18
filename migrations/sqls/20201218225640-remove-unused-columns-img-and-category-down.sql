ce550a04b80492d1441419d88b8e90dd2b90cf2b
Igor Golovin <lislon@mail.ru>
2020-12-19 02:43:48 +0300

    Fix down migration

--- migrations/sqls/20201218225640-remove-unused-columns-img-and-category-down.sql
+++ migrations/sqls/20201218225640-remove-unused-columns-img-and-category-down.sql
@@ -2,4 +2,4 @@ ALTER TABLE cb_events ADD COLUMN subcategory text NOT NULL DEFAULT '';
 ALTER TABLE cb_events_snapshot ADD COLUMN subcategory text NOT NULL DEFAULT '';
 
 ALTER TABLE cb_events_packs ADD COLUMN image bytea;
-ALTER TABLE cb_events_packs ADD COLUMN image_src text NOT NULL DEFAULT '',
+ALTER TABLE cb_events_packs ADD COLUMN image_src text NOT NULL DEFAULT '';
