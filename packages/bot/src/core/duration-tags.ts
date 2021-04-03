88b95ebc2c150bc25964a9e9101b554e7b08cdad
Igor Golovin <lislon@mail.ru>
2021-04-04 00:23:54 +0300

    Fix bug with open interval duration and tag #1hour
    For example: 'from 2 hours' -> #1hour

--- packages/bot/src/core/duration-tags.ts
+++ packages/bot/src/core/duration-tags.ts
@@ -8,10 +8,14 @@ export function autoAppendDurationTags(existingTags: TagLevel2[], data: Event, d
     }
 
     if (duration !== 'unknown') {
-        const maxMinutes = max(duration.map(d => d.max)) / 60;
+        const hasInfitive = duration.find(d => d.max === undefined)
 
-        if (maxMinutes <= 60) {
-            return [...existingTags.filter(s => s !== '#успетьзачас'), '#успетьзачас']
+        if (!hasInfitive) {
+            const maxDurations = duration.map(d => d.max)
+            const maxMinutes = max(maxDurations) / 60;
+            if (maxMinutes <= 60) {
+                return [...existingTags.filter(s => s !== '#успетьзачас'), '#успетьзачас']
+            }
         }
     }
 
